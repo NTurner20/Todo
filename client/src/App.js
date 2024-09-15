3ba81719e2b61cb4e1d3c3046495c99cf9b09ba1
Nick Turner <ncttcn@gmail.com>
2024-09-15 14:29:28 -0500

    react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+
+// components
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
+
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <div className="container">
+      <Routes>
+        <Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to="/dashbaord" />}/>
+        <Route path="/register" element={!isAuthenticated ? <Register /> : <Navigate to="/login" />}/>
+        <Route path="/dashboard" element={isAuthenticated ?<Dashboard /> : <Navigate to="/login"/>}/>
+      </Routes>
+      </div>
+
+    </Router>
+    </>
   );
 }
 
